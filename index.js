1ca858884b002417886ceaf77784d382cef0fe8d
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-17 22:48:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ function add(a,b) {
 return a + b
 }
 
+function subtract(a,b) {
+return a - b
+}
+
+function multiply(a,b) {
+return a * b
+}
+
+function divide(a,b) {
+return a / b
+}
+
+function inc(n) {
+n++;
+}
+
+function dec(n) {
+  n--
+}
+
